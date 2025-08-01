61b9cc428f5a9c2d3881bd4c2672ab2deb17b5b7
李晨熙 <chenxili380@gmail.com>
2025-08-02 07:43:45 +0800

    1

--- blogFrontend/src/hooks/useDebounce.ts
+++ blogFrontend/src/hooks/useDebounce.ts
@@ -4,12 +4,10 @@ export function useDebounce<T>(value: T, delay: number): T {
   const [debouncedValue, setDebouncedValue] = useState<T>(value);
 
   useEffect(() => {
-    // 设置定时器
     const timer = setTimeout(() => {
       setDebouncedValue(value);
     }, delay);
 
-    // 清理函数：在 value 或 delay 改变时，清除之前的定时器
     return () => {
       clearTimeout(timer);
     };
