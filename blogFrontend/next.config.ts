277716ba434fc48b513b989fd91a6cffce99b09b
李晨熙 <chenxili380@gmail.com>
2025-08-01 12:52:31 +0800

    Update next.config.ts

--- blogFrontend/next.config.ts
+++ blogFrontend/next.config.ts
@@ -16,7 +16,7 @@ const nextConfig: NextConfig = {
                 protocol: 'https',
                 hostname: 'images-1359353257.cos.ap-beijing.myqcloud.com',
                 port: '',
-                pathname: '/images/**',
+                pathname: '/**',
             },
         ],
     },
