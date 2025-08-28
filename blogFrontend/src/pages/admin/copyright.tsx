8b6bb595ad0b73030148b7acb156fb999b6149bb
李晨熙 <chenxili380@gmail.com>
2025-08-28 09:43:42 +0800

    Update copyright.tsx

--- blogFrontend/src/pages/admin/copyright.tsx
+++ blogFrontend/src/pages/admin/copyright.tsx
@@ -2,11 +2,18 @@ import React from 'react';
 import AdminLayout from '@/admin/components/layout/AdminLayout';
 import CopyrightManagement from '@/admin/components/CopyrightManagement/CopyrightManagement';
 import { withAdminAuth } from '@/admin/components/withAdminAuth';
+import Head from "next/head";
 
 const CopyrightPage: React.FC = () => {
   return (
     <AdminLayout>
-      <CopyrightManagement />
+      <div>
+        <Head>
+          <title>管理员页面 | 版权管理</title>
+          <meta name="description" />
+        </Head>
+        <CopyrightManagement />
+      </div>
     </AdminLayout>
   );
 };
