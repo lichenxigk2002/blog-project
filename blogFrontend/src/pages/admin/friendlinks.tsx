3e06dfd30b32327d89d169ada04ea3c1d65d45c9
李晨熙 <chenxili380@gmail.com>
2025-08-28 09:43:46 +0800

    Update friendlinks.tsx

--- blogFrontend/src/pages/admin/friendlinks.tsx
+++ blogFrontend/src/pages/admin/friendlinks.tsx
@@ -2,11 +2,19 @@ import React from 'react';
 import AdminLayout from '@/admin/components/layout/AdminLayout';
 import FriendLinksManagement from '@/admin/components/FriendLinksManagement/FriendLinksManagement';
 import { withAdminAuth } from '@/admin/components/withAdminAuth';
+import Head from "next/head";
 
 const Friendlinks: React.FC = () => {
     return (
         <AdminLayout>
-            <FriendLinksManagement />
+            <div>
+                <Head>
+                    <title>管理员页面 | 版权管理</title>
+                    <meta name="description" />
+                </Head>
+                <FriendLinksManagement />
+            </div>
+
         </AdminLayout>
     );
 };
