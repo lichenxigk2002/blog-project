da3bcd38ec54e916228b9f30a355b3bcb96cebc2
李晨熙 <chenxili380@gmail.com>
2025-08-28 09:43:50 +0800

    Create rss.tsx

---
+++ blogFrontend/src/pages/admin/friendlinks/rss.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import AdminLayout from '@/admin/components/layout/AdminLayout';
+import RssManagement from '@/admin/components/FriendLinksManagement/RssManagement';
+import Head from 'next/head';
+
+const RssManagementPage: React.FC = () => {
+  return (
+    <>
+      <Head>
+        <title>RSS管理 - 博客管理后台</title>
+        <meta name="description" content="管理RSS源和友链关联" />
+      </Head>
+
+      <AdminLayout>
+        <RssManagement />
+      </AdminLayout>
+    </>
+  );
+};
+
+export default RssManagementPage; 
