cb89327b8d021e9c51c983adf476f28dd6418167
李晨熙 <chenxili380@gmail.com>
2025-10-13 16:41:49 +0800

    Update Button.tsx

--- blogFrontend/src/admin/components/ui/Button/Button.tsx
+++ blogFrontend/src/admin/components/ui/Button/Button.tsx
@@ -17,7 +17,6 @@ const Button: React.FC<ButtonProps> = ({
 }) => {
   const isIconOnly = icon && !children;
 
-
   return (
     <button
       className={`${styles.button} ${styles[variant]} ${styles[size]} ${isIconOnly ? styles.iconOnly : ''} ${className}`}
